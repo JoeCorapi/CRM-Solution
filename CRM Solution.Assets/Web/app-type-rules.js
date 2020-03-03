66672aa12ce587fd38cbbefccb6fab0138c3aa88
Joe Corapi <joe.corapi@smoothstack.com>
2020-03-03 14:17:18 -0500

    Move app-type js to correct scope

---
+++ CRM Solution.Assets/Web/app-type-rules.js
@@ -0,0 +1,11 @@
+﻿function crmAlert(Context){
+
+    let myUniqueId = "_myUniqueId"; // Define an ID for the notification
+    let currentUserName = Xrm.Utility.getGlobalContext().userSettings.userName; // get current user name
+    let message = currentUserName + ": Your JavaScript code in action!";
+
+    alert(message);
+
+    Context.
+}
+
