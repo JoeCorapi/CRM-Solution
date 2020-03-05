0f11166becfbf90a666bf9ebc205a795add14ea7
Joe Corapi <joe.corapi@smoothstack.com>
2020-03-05 18:03:42 -0500

    add custom ribbon button script

---
+++ CRM Solution.Assets/Web/Ribbons.js
@@ -0,0 +1,5 @@
+﻿function CancelApplication(primaryControl) {
+    var formContext = primaryControl; 
+    formContext.getAttribute("statecode").setValue(1);
+    formContext.getAttribute("statuscode").setValue(2);
+}
