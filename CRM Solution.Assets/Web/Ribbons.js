d8995940a96e53f27bc9e8c580f51e171e58eacb
Joe Corapi <24542019+JoeCorapi@users.noreply.github.com>
2020-03-05 19:40:37 -0500

    comments for clarity

--- CRM Solution.Assets/Web/Ribbons.js
+++ CRM Solution.Assets/Web/Ribbons.js
@@ -1,6 +1,7 @@
 ﻿function CancelApplication(primaryControl) {
     let formContext = primaryControl; 
-    formContext.getAttribute("statecode").setValue(1);
-    formContext.getAttribute("statuscode").setValue(2);
+
+    formContext.getAttribute("statecode").setValue(1); //deactivated
+    formContext.getAttribute("statuscode").setValue(2); //"Inactive"
     formContext.data.entity.save();
 }
