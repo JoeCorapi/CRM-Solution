2b90439b767bcc24d0b63eda9a761dd13805be2d
Joe Corapi <24542019+JoeCorapi@users.noreply.github.com>
2020-03-05 19:32:58 -0500

    add save on state change

--- CRM Solution.Assets/Web/Ribbons.js
+++ CRM Solution.Assets/Web/Ribbons.js
@@ -1,5 +1,6 @@
 ﻿function CancelApplication(primaryControl) {
-    var formContext = primaryControl; 
+    let formContext = primaryControl; 
     formContext.getAttribute("statecode").setValue(1);
     formContext.getAttribute("statuscode").setValue(2);
+    formContext.data.entity.save();
 }
